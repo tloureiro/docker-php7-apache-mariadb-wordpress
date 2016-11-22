e0a2b58557de09ba709a1aa1a393b3d9812388d6
thiagoloureiro <thiago@artscience.ca>
2016-11-22 18:06:58 -0500

    admin user can access the db from localhost

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ mysql_install_db;
 
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin'";
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION";
+mysql -uroot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'";
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION";
 
 chmod -R 777 /var/lib/mysql;
 chmod -R 777 /dump;
